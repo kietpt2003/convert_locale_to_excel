3478efac50b4971447b65a5afba6daa98eddb81e
kietpt2003 <tuankiet29012003@gmail.com>
2026-06-13 22:05:06 +0700

    [0.0.7][master] - Add api check order status

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { verifyToken } from '../middleware/validation.js';
 import {
+  checkOrderStatus,
   createSePayQROrder,
   handleActivatePlan,
   handlePaypalSuccess,
@@ -14,5 +15,6 @@ router.post('/paypal-success', verifyToken, handlePaypalSuccess);
 router.post('/premium/activate-plan', verifyToken, handleActivatePlan);
 router.post('/webhook/se-pay', handleSePayWebhook);
 router.post('/se-pay', verifyToken, createSePayQROrder);
+router.get('/status/:orderCode', verifyToken, checkOrderStatus);
 
 export default router;
